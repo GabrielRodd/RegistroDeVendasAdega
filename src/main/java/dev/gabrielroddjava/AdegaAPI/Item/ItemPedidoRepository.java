5c4cb0b98ecb07ee72b706b34be2f6f4aee5fa16
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-19 12:07:26 -0300

    feat: Cria os Respositories das classes

---
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Item/ItemPedidoRepository.java
@@ -0,0 +1,6 @@
+package dev.gabrielroddjava.AdegaAPI.Item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedidoModel, Long> {
+}
---
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Pedidos/PedidoRepository.java
@@ -0,0 +1,6 @@
+package dev.gabrielroddjava.AdegaAPI.Pedidos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
