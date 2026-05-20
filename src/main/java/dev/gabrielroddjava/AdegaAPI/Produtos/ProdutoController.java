02503a446f621f5793a3b9c07a1a6f7c7871d3ab
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-20 14:31:27 -0300

    feat: Cria os endpoints de GET e POST no ProdutoController e adiciona lógicas para retornos de rota para todos os metodos no ProdutoService

--- src/main/java/dev/gabrielroddjava/AdegaAPI/Produtos/ProdutoController.java
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Produtos/ProdutoController.java
@@ -1,15 +1,35 @@
 package dev.gabrielroddjava.AdegaAPI.Produtos;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    //Injetando service
+    ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+
     @GetMapping("boasvindas")
     public String boasVindasTeste() {
         return "boas vindas a rota";
+    }
+
+    //GET - Mostrar todos produtos
+    @GetMapping("/mostrar")
+    public List<ProdutoModel> mostrarTodosProdutos() {
+        return produtoService.listarProdutos();
+    }
+
+    //POST - Criar Produto
+    @PostMapping("/criar")
+    public ProdutoModel criarProduto(@RequestBody ProdutoModel novoProduto) {
+        return produtoService.cadastrarNovoProduto(novoProduto);
     }
 }
--- src/main/java/dev/gabrielroddjava/AdegaAPI/Produtos/ProdutoService.java
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Produtos/ProdutoService.java
@@ -1,4 +1,43 @@
 package dev.gabrielroddjava.AdegaAPI.Produtos;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProdutoService {
+
+    //Injetando dependencia do Repository
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoService (ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public ProdutoModel cadastrarNovoProduto(ProdutoModel novoProduto) {
+        return produtoRepository.save(novoProduto);
+    }
+
+    public List<ProdutoModel> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public ProdutoModel listarProdutoPorId(Long id) {
+        Optional<ProdutoModel> produtoBuscado = produtoRepository.findById(id);
+        return produtoBuscado.orElse(null);
+    }
+
+    public void deletarProdutoPorId(Long id) {
+        ProdutoModel produtoDeletar = listarProdutoPorId(id);
+        produtoRepository.delete(produtoDeletar);
+    }
+
+    public ProdutoModel atualizarProdutoPorId(Long id, ProdutoModel produtoAtualizado) {
+        if (produtoRepository.existsById(id)) {
+            produtoAtualizado.setId(id);
+        }
+        return produtoRepository.save(produtoAtualizado);
+    }
+
 }
