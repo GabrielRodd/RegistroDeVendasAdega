10a4afe619cfe276f59c4a74935327b97aa401bd
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-20 12:17:26 -0300

    feat: Cria os endpoints do PedidoController, apenas para teste, sem funcionalidades ainda

--- src/main/java/dev/gabrielroddjava/AdegaAPI/Pedidos/PedidoController.java
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Pedidos/PedidoController.java
@@ -1,4 +1,39 @@
 package dev.gabrielroddjava.AdegaAPI.Pedidos;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
+
+    //GET - Mostrar todos os pedidos
+    @GetMapping("/mostrar")
+    public String mostrarTodosPedidos() {
+        return "mostrando todos os pedidos";
+    }
+
+    //GET - Mostrar pedidos por ID
+    @GetMapping("/mostrarporid")
+    public String mostrarPedidoPorId() {
+        return "mostrando por id";
+    }
+
+    //POST - Criar novo pedido
+    @PostMapping("/criar")
+    public String criarPedido(){
+        return "criando pedido";
+    }
+
+    //PUT - Atualizar pedido
+    @PutMapping("/editar")
+    public String editarPedido() {
+        return "editando pedido";
+    }
+
+    //DELETE - Deletar pedido
+    @DeleteMapping("/deletar")
+    public String deletarPedido() {
+        return "Deletando pedido";
+    }
+
 }
