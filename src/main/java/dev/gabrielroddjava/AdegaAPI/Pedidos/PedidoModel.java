6dd771079cf06f32139ecba0663e0381f90fba6e
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-27 11:37:29 -0300

    refactor: Adiciona o toString nas classes Model

--- src/main/java/dev/gabrielroddjava/AdegaAPI/Pedidos/PedidoModel.java
+++ src/main/java/dev/gabrielroddjava/AdegaAPI/Pedidos/PedidoModel.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class PedidoModel {
 
     @Id
